6ea15423920d102e30ec2d72bb1185dd359f30d3
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-12-09 21:50:13 +0300

    hot fix 05

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""
+Defines the BaseModel class.
+"""
+
 import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Defines the base model for other classes."""
+    """
+    Defines the base model for other classes.
+
+    Attributes:
+        id (str): The unique identifier for the instance.
+        created_at (datetime): The date and time the instance was created.
+        updated_at (datetime): The date and time the instance was last updated.
+    """
+
     def __init__(self, *args, **kwargs):
         """
         Initialize the BaseModel instance.
@@ -23,10 +34,12 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
+                        # Convert date strings to datetime objects
                         date_format = '%Y-%m-%dT%H:%M:%S.%f'
                         value = datetime.strptime(value, date_format)
                     setattr(self, key, value)
         else:
+            # Generate new id, created_at, and updated_at values
             self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -70,7 +83,7 @@ class BaseModel:
     def save(self):
         """
         Update the public instance attribute updated_at with the current
-        datetime.
+        datetime and save to the JSON file.
         """
         self.updated_at = datetime.now()
         models.storage.save()
